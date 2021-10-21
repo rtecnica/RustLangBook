f1e0e3528278fad2a701ff4ecade1ea14170fe14
rtecnica <imaldonado@ing.uchile.cl>
2021-10-21 14:06:07 +0000

    Completed guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,52 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut tries = 0;
+    let max_tries = 7;
+
+    while tries < max_tries {
+        println!("Guess the secret number:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        tries = tries + 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guess it! {}", secret_number);
+                break;
+            }
+        }
+
+        println!("Tries left: {}", max_tries - tries);
+    }
+    if tries == max_tries {
+        println!("Out of tries!");
+    }
 }
