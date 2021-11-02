1f42560f6cc82afe16550982fb75f084834a4012
rtecnica <imaldonado@ing.uchile.cl>
2021-11-01 22:57:15 -0300

    Added description of exersize

--- chapter3/src/homework.rs
+++ chapter3/src/homework.rs
@@ -1,11 +1,10 @@
 /*
- *
- *
- *
- *
- *
- *
- *
+ * If you want to practice with the concepts discussed in this chapter, try build-
+ * ing programs to do the following:
+ *  - Convert temperatures between Fahrenheit and Celsius.
+ *  - Generate the nth Fibonacci number.
+ *  - Print the lyrics to the Christmas carol “The Twelve Days of Christmas,”
+ * taking advantage of the repetition in the song.
  */
 
 
--- chapter8/stats/src/main.rs
+++ chapter8/stats/src/main.rs
@@ -1,4 +1,9 @@
-
+/*
+ * Given a list of integers, use a vector and return the mean (the average
+ * value), median (when sorted, the value in the middle position), and
+ * mode (the value that occurs most often; a hash map will be helpful
+ * here) of the list.
+ */
 
 mod stats;
 
